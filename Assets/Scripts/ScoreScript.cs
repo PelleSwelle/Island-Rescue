c0f7c103d9ef4cd5afd81a01a696e8484d63b017
chris6892 <70691056+chris6892@users.noreply.github.com>
2023-03-20 23:59:43 +0100

    Working NPC pickup interaction and score goes up

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 
 public class ScoreScript : MonoBehaviour
 {
 
+    
     public TextMeshProUGUI scoreValue;
-    private int score;
+    public static int score;
     
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
-        scoreValue.text = "Score: " + score;
+        scoreValue.text = "Score: " + score.ToString();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        scoreValue.text = "Score: " + score.ToString();
     }
 }
