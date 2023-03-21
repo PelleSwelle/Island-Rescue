6d03e784a3d7277fcf9f96d2f1c1aa73f9740f83
Peter Dønvang <72729955+PelleSwelle@users.noreply.github.com>
2023-03-21 15:43:34 +0100

    implement audio source and listener

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour
 {
-   public void Interact() {
-     Destroy(gameObject);
-     ScoreScript.score++;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
+    public AudioClip clip4;
+    public AudioClip clip5;
+    public AudioSource source;
+    float timeToScream;
+    public void Interact()
+    {
+        Destroy(gameObject);
+        ScoreScript.score++;
+    }
+
+    void Update()
+    {
+        timeToScream = Random.Range(1, 50);
+        playVoiceAtRandomTime();
+    }
+
+
+    void playVoiceAtRandomTime()
+    {
+        if (!source.isPlaying) // check if playing already
+        {
+            if ((int)timeToScream == 3) // random value TODO: change this
+            {
+                source.PlayOneShot(clip1);
+            }
+        }
     }
 }
