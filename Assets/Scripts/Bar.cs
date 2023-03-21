6ef1726f19e9468589e153f3b9251b4167dee266
avclark2 <aclark20@student.aau.dk>
2023-03-21 14:53:23 +0100

    Water Level bar
    Made it work  with the rest of the game elements

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -5,22 +5,37 @@ using DentedPixel;
 
 public class Bar : MonoBehaviour {
     
-    public GameObject bar;
+    public RectTransform bar;
     public int time;
+    public Transform Water;
+
 
 
       
     void Start () {
         AnimateBar();
-    }
+           }
 
 
        void Update () {
-
+        updateBar();
         }
 
     public void AnimateBar()
             {
-              LeanTween.scaleX(bar, 1, time);
-        }
+               
+       }
+
+    public static float mapRange(float inStart, float inEnd, float outStart, float outEnd, float value)
+    {
+        float scale = (outEnd - outStart) / (inEnd - inStart);
+        return (outStart + ((value - inStart) * scale));
     }
+    void updateBar()
+    {
+        float WaterLevel = Water.position.y;
+        float BarLevel = mapRange(0f, 100f, -429f, 0, WaterLevel);
+        bar.offsetMax = new Vector2(bar.offsetMax.x, BarLevel);
+    }
+
+}
