a92e6123b8439a9afd4c2da6238fdf52615a718b
Peter Dønvang <72729955+PelleSwelle@users.noreply.github.com>
2023-04-25 21:53:12 +0200

    put things on npcs

---
+++ Assets/Scripts/VillagerBehavior.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerBehavior : MonoBehaviour
+{
+    public AudioSource source;
+    public List<AudioClip> shouts;
+
+    public Transform water;
+    public Stamina stamina;
+    public bool isDead, hasBeenRescued = true;
+    
+    float timeToScream;
+
+    bool isUnderWater = false;
+
+    void Awake() 
+    {
+        water = FindObjectOfType<RisingScript>().GetComponent<Transform>();
+        source = GetComponent<AudioSource>();
+        stamina = GetComponent<Stamina>();
+    }
+
+    public void Start()
+    {
+        stamina.currentStamina = stamina.maxStamina;
+        isDead = false;
+        hasBeenRescued = false;
+    }
+    
+    void runForExtraction()
+    {
+        // do pathfinding shit
+    }
+
+    void Update()
+    {
+        isDead = stamina.isEmpty;
+        isUnderWater = transform.position.y < water.position.y;
+        timeToScream = Random.Range(1, 70);
+
+        if (hasBeenRescued) 
+            runForExtraction();
+
+        if (!isDead && !hasBeenRescued)
+        {
+            if (isUnderWater)
+                stamina.decrease();
+            
+            playVoiceAtRandomTime();
+        }
+        if (isDead)
+            transform.rotation = new Quaternion(0, 0, 90, 0);
+    }
+
+    void playVoiceAtRandomTime()
+    {
+        if (!source.isPlaying) // check if playing already
+            if ((int)timeToScream == 3) // random value TODO: change this
+                source.PlayOneShot(shouts[Random.Range(0, shouts.Count)]);
+    }
+}
