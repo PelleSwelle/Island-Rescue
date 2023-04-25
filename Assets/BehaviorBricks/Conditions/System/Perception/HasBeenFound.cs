4da2c4d0eb1e7e6dddf053a67bc2f5af96a5c488
Peter Dønvang <72729955+PelleSwelle@users.noreply.github.com>
2023-04-25 15:54:09 +0200

    status

---
+++ Assets/BehaviorBricks/Conditions/System/Perception/HasBeenFound.cs
@@ -0,0 +1,13 @@
+using Pada1.BBCore;
+using UnityEngine;
+
+namespace BBUnity.Conditions
+{
+    public class HasBeenFound : GOCondition
+    {
+        public override bool Check()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
